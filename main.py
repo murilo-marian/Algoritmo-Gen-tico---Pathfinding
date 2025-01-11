3d9c3b1808ccb4c4d9b23e6c8d1ef44c6a081183
murilo-marian <murilomarian.mm@gmail.com>
2025-01-11 17:34:47 -0300

    de forma geral a função de percorrer_caminho está funcional, agora é necessário adicionar o contador de peso do caminho que vai ser usado pra avaliar a fitness

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+from json.encoder import INFINITY
+from operator import truediv
+
 import numpy as np
 
 # parâmetros
@@ -10,15 +13,16 @@ TAXA_MUTACAO = 0.005
 #genes: left/right preference binary; ignore obstacle weight threshold; ignore obstacle distance threshold
 
 mapa = np.array([[random.randint(0, 9) for _ in range(5)] for _ in range(5)], dtype=object)
-mapa = np.array([[1, 4, 1, 1],
-                 [1, 5, 7, 1],
-                 [1, 1, 6, 8],
-                 [1, 1, 1, 4]], dtype=object)
-pos_inicial = [0, 0]
-pos_alvo = [3, 3]
+mapa = np.array([[0, 8, 1, 2, 5],
+                  [3, 8, 4, 9, 9],
+                  [4, 7, 1, 9, 1],
+                  [5, 4, 2, 1, 9],
+                  [5, 3, 5, 3, 0]], dtype=object)
+pos_inicial = [2, 0]
+pos_alvo = [4, 4]
 
 
-def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, delta_maximo):
+def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, delta_maximo, left_right_preference, up_down_preference):
     pos_atual = list(pos_inicial)
     visitados = set()
 
@@ -28,18 +32,19 @@ def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, d
     matriz[pos_atual[0], pos_atual[1]] = "X"
 
     print_matriz_formatada_np(matriz)
-    print("----------------")
 
     while tuple(pos_atual) != tuple(pos_final):
         distancia_x = abs(pos_atual[0] - pos_final[0])
         distancia_y = abs(pos_atual[1] - pos_final[1])
 
-
+        movimentos_validos = obter_movimentos_validos(matriz, pos_atual, visitados)
+        print(movimentos_validos)
+        print("----------------")
         #primeiro teste é se ele tá perto o suficiente do alvo pra só ignorar os pesos completamente
         #caso não esteja, ele faz todos os outros testes
         if distancia_x <= distancia_ignorar_pesos and distancia_y <= distancia_ignorar_pesos:
             #mover normal
-            pos_atual = movimentacao_base(distancia_x, distancia_y, pos_atual, pos_final)
+            pos_atual = movimentacao_base(distancia_x, distancia_y, pos_atual, pos_final, movimentos_validos)
         else:
             #fazer os outros testes pra decidir onde ele vai seguir
             #o algoritmo tem que levar em consideração todos os genes que eu escolhi
@@ -56,27 +61,24 @@ def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, d
             #a diferença entre apenas seguir para a direita e subir for pequena o suficiente, o algoritmo
             #vai dar preferência a seguir para a direita ao invés de desviar para cima
 
-            movimentos_validos = obter_movimentos_validos(matriz, pos_atual, visitados)
-            print(movimentos_validos)
-
             if movimentos_validos:
                 movimentos_validos.sort(key=lambda x: x[0])
 
                 #aqui ele vai ter que fazer os testes do gene 1, 2 e 4
 
                 #primeira coisa a se fazer é determinar qual direção ele seguiria se fosse a movimentação normal
-                direcao = [0, 0]
-                if distancia_x > distancia_y:
-                    direcao[0] += 1 if pos_atual[0] < pos_final[0] else -1
-                else:
-                    direcao[1] += 1 if pos_atual[1] < pos_final[1] else -1
+                direcao_x = [0, 0]
+                direcao_y = [0, 0]
+                direcao_x[0] += 1 if pos_atual[0] < pos_final[0] else -1
+                direcao_y[1] += 1 if pos_atual[1] < pos_final[1] else -1
 
                 #agora o algoritmo deve determinar se essa direção é viável, se não, ele vai escolher outra para desviar
 
                 #se a melhor direção já for a com o menor peso, só vai
                 #TODO adicionar checagem de se outros valores do array movimentos_validos tem peso igual
                 #TODO já que ele só testa o primeiro elemento, e se o sort deixou o elemento optimo em segundo ele não vai funcionar direito
-                if direcao == [a - b for a, b in zip(movimentos_validos[0][1], pos_atual)]:
+
+                if direcao_x == [a - b for a, b in zip(movimentos_validos[0][1], pos_atual)] or direcao_y == [a - b for a, b in zip(movimentos_validos[0][1], pos_atual)]:
                     proxima_pos = movimentos_validos[0][1]
                     pos_atual = list(proxima_pos)
                 else:
@@ -85,16 +87,57 @@ def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, d
                     #se a direção certa tá dentro do delta, manda bala
                     #caso contrário, desvia pro caminho de menos resistência
                     #caso tenha dois empatados, aí entra em jogo o gene 1 e 2
-                    nova_pos = tuple([x + y for x, y in zip(direcao, pos_atual)])
-                    if any(nova_pos == movimento for _, movimento in movimentos_validos):
-                        indice = next((i for i, (_, movimento) in enumerate(movimentos_validos) if movimento == nova_pos), None)
-                        diferenca_de_pesos = abs(movimentos_validos[0][0] - movimentos_validos[indice][0])
-                        if diferenca_de_pesos < delta_maximo:
-                            #se a diferença for pequena o suficiente, ignora e segue o baile
-                            pos_atual = movimentacao_base(distancia_x, distancia_y, pos_atual, pos_final)
+                    nova_pos_x = tuple([x + y for x, y in zip(direcao_x, pos_atual)])
+                    nova_pos_y = tuple([x + y for x, y in zip(direcao_y, pos_atual)])
+                    if any(nova_pos_x == movimento or nova_pos_y == movimento for _, movimento in movimentos_validos):
+                        indice_x = next((i for i, (_, movimento) in enumerate(movimentos_validos) if movimento == nova_pos_x), None)
+                        indice_y = next((i for i, (_, movimento) in enumerate(movimentos_validos) if movimento == nova_pos_y), None)
+                        diferenca_de_pesos_x = INFINITY
+                        diferenca_de_pesos_y = INFINITY
+                        if indice_x:
+                            diferenca_de_pesos_x = abs(movimentos_validos[0][0] - movimentos_validos[indice_x][0])
+                        elif indice_y:
+                            diferenca_de_pesos_y = abs(movimentos_validos[0][0] - movimentos_validos[indice_y][0])
+                        if diferenca_de_pesos_x <= delta_maximo:
+                            pos_atual = nova_pos_x
+                        elif diferenca_de_pesos_y <= delta_maximo:
+                            pos_atual = nova_pos_y
                         else:
-                            #desvio maroto
-                            print("teste")
+                            #primeiro ele vê o peso dos caminhos disponíveis pra desvio
+                            #e vai escolher o mais leve
+                            #caso tiver mais de um peso mais leve, e não tiver pendendo pra um lado em específico, ele vai usar o gene 1/2 pra decidir qual vai escolher
+                            menor_peso = movimentos_validos[0][0]
+                            movimentos_com_menor_peso = [movimento for movimento in movimentos_validos if
+                                                         movimento[0] == menor_peso]
+
+                            if len(movimentos_com_menor_peso) > 1:
+                                movimentos_opostos = []
+                                for i, mov1 in enumerate(movimentos_com_menor_peso):
+                                    if movimentos_opostos:
+                                        break
+                                    for j, mov2 in enumerate(movimentos_com_menor_peso):
+                                        if i != j and movimentos_sao_opostos(mov1[1], mov2[1], pos_atual):
+                                            movimentos_opostos = [mov1, mov2]
+                                            break
+
+                                if movimentos_opostos:
+                                    mov1, mov2 = movimentos_opostos
+                                    left_right_preference_new_position = [a + b for a, b in zip(pos_atual, left_right_preference)]
+                                    up_down_new_position = [a + b for a, b in zip(pos_atual, up_down_preference)]
+                                    if left_right_preference_new_position == list(mov1[1]) or left_right_preference_new_position == list(mov2[1]):
+                                        pos_atual = left_right_preference_new_position
+                                    else:
+                                        pos_atual = up_down_new_position
+                                else:
+                                    # Caso não sejam opostos ou só há um movimento válido
+                                    pos_atual = movimentos_com_menor_peso[0][1]
+                            else:
+                                # Apenas um movimento com menor peso
+                                pos_atual = movimentos_com_menor_peso[0][1]
+                    else:
+                        print("sem caminho disponível - preso")
+                        break
+
             else:
                 print("Sem movimentos válidos! Caminho bloqueado.")
                 break
@@ -107,12 +150,29 @@ def percorrer_caminho(matriz, pos_inicial, pos_final, distancia_ignorar_pesos, d
         print("----------------")
 
 
-def movimentacao_base(distancia_x, distancia_y, pos_atual, pos_final):
-    if distancia_x > distancia_y:
-        pos_atual[0] += 1 if pos_atual[0] < pos_final[0] else -1
+def movimentacao_base(distancia_x, distancia_y, pos_atual, pos_final, movimentos_validos):
+    nova_pos_x = pos_atual[:]
+    nova_pos_y = pos_atual[:]
+
+    nova_pos_x[0] += 1 if pos_atual[0] < pos_final[0] else -1
+    nova_pos_y[1] += 1 if pos_atual[1] < pos_final[1] else -1
+
+    if nova_pos_x == pos_final:
+        return nova_pos_x
+    elif nova_pos_y == pos_final:
+        return nova_pos_y
+
+    if any(tuple(nova_pos_x) == movimento for _, movimento in movimentos_validos):
+        return nova_pos_x
     else:
-        pos_atual[1] += 1 if pos_atual[1] < pos_final[1] else -1
-    return pos_atual
+        return nova_pos_y
+
+def movimentos_sao_opostos(mov1, mov2, pos_atual):
+    primeiro = [abs(a - b) for a, b in zip(mov1, pos_atual)]
+    segundo = [abs(a - b) for a, b in zip(mov2, pos_atual)]
+    if primeiro == segundo:
+        return True
+    return False
 
 def obter_movimentos_validos(matriz, pos_atual, visitados):
     movimentos = [
@@ -200,4 +260,4 @@ def print_matriz_formatada_np(matriz):
 # print(f"genes da melhor solução: {melhor_solucao}")
 # print(f"Fitness : {avaliar_fitness(melhor_solucao)}")
 
-percorrer_caminho(mapa, pos_inicial, pos_alvo, 0, 2)
+percorrer_caminho(mapa, pos_inicial, pos_alvo, 1, 3, [0, 1], [1, 0])
